a04c9ce9a344eef9ecffc8d41fe7b1f25431fbc6
Graham McBain <grahammcbain@gmail.com>
2015-07-06 10:31:07 -0600

    simple webhook server

--- webhook_server.rb
+++ webhook_server.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+require 'JSON'
+
 
 post '/test' do
 	'Nice Webhook Skills!'
+
+	# request.body.rewind
+	#  request_payload = JSON.parse(request.body.read)
+  
+  
+ #  File.open("response.txt", "a") do |f|
+ #    f.puts(JSON.pretty_generate(request_payload))
+ #  end
 end
+
